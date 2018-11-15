bc5af919e16c91811111266c8a955fa7f67984d7
sabotior <black_lion@mail.ru>
2018-11-15 06:13:25 +0400

    minCSS

--- lesson7/gulpfile.js
+++ lesson7/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
 var browserSync = require('browser-sync');
 
 gulp.task('scss', function() {
@@ -8,6 +9,14 @@ gulp.task('scss', function() {
 .pipe(gulp.dest('src/css'))//сохранили
 .pipe(browserSync.reload({ stream: true}));//поток
 });
+
+gulp.task('minify-css', () => {
+  return gulp.src('src/css/*.css')
+   .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('src/mincss'))
+   .pipe(browserSync.reload({ stream: true}));//поток
+});
+
 gulp.task('browserSync', function() {
   browserSync({
     server: {
@@ -18,5 +27,6 @@ gulp.task('browserSync', function() {
 
 gulp.task('watch', ['browserSync'], function() {//контроль изменений (при сохранении) можно несколько команд в массиве 'watch', [''], function()
   gulp.watch('src/scss/**/*.+(scss|sass)', ['scss']);//путь-шаблон, [задачи которые нужно запустить]
+  gulp.watch('src/css/*.css', ['minify-css']);//путь-шаблон, [задачи которые нужно запустить]
   gulp.watch('src/index.html', browserSync.reload);
 });
