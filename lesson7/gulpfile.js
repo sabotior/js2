d0ee0fbc6147fd54760d359ce7266e9820dc4118
sabotior <black_lion@mail.ru>
2018-11-15 05:01:16 +0400

    вся лекция, работает

--- lesson7/gulpfile.js
+++ lesson7/gulpfile.js
@@ -1,12 +1,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
 
 gulp.task('scss', function() {
-  return gulp.src('src/scss/**/*.+(scss|sass)')
-.pipe(sass())
-.pipe(gulp.dest('src/css'));
+  return gulp.src('src/scss/**/*.+(scss|sass)')//прочитали
+.pipe(sass())//обработали
+.pipe(gulp.dest('src/css'))//сохранили
+.pipe(browserSync.reload({ stream: true}));//поток
+});
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'src',
+    }
+  })
 });
 
-gulp.task('watch', function() {
-  gulp.watch('src/scss/**/*.+(scss|sass)', ['scss']);
+gulp.task('watch', ['browserSync'], function() {//контроль изменений (при сохранении) можно несколько команд в массиве 'watch', [''], function()
+  gulp.watch('src/scss/**/*.+(scss|sass)', ['scss']);//путь-шаблон, [задачи которые нужно запустить]
+  gulp.watch('src/index.html', browserSync.reload);
 });
